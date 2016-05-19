79fd403fadad358dbd84e1c7a1337ab09761193d
Krypson Technologies <pmishra@krypson.com>
2016-05-19 18:39:28 +0530

    Added resource collection

---
+++ app/code/local/Krypson/Testimonials/Model/Resource/Testimonial/Collection.php
@@ -0,0 +1,8 @@
+<?php
+	class Krypson_Testimonials_Model_Resource_Testimonial_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract {
+
+		protected function _construct() {
+			$this->_init('testimonials/testimonial');
+		}
+
+	}
